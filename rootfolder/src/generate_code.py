dfb05d4dd112381724461eaf66a520e909bd09f6
RushaanRajeshPai <pairushaan64@gmail.com>
2025-03-21 02:07:57 +0530

    1st commit

---
+++ rootfolder/src/generate_code.py
@@ -0,0 +1,21 @@
+# from langchain_community.chat_models import ChatGoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.schema import SystemMessage, HumanMessage
+import os
+
+os.environ["GOOGLE_API_KEY"] = "AIzaSyDQcmV2pdj6Z2gSlCrWHfjojwyfOlBIv0Y"
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+user_query = "Generate a Python script to plot a bar chart showing year vs number of job losses using Matplotlib and save it as 'output/graph.png'"
+messages = [
+    SystemMessage(content="You are an expert Python developer. Generate correct and executable Python code."),
+    HumanMessage(content=user_query),
+] #LLM prompt
+
+response = llm(messages)
+generated_code = response.content
+
+file_path = "src/generated_graph.py"
+with open(file_path, "w") as f:
+    f.write(generated_code)
+
+print(f"Python code generated and saved to {file_path}")
